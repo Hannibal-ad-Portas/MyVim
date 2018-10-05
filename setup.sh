26076bcb2458376194ada0801c3d069642b25bfc
Hannibal-ad-Portas <jconroy12@gmail.com>
2018-10-05 18:17:53 -0400

    Improved instation experience
    Install script creats a symbolic link, downloads vundle, and installs
    all other plugins

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+################################################################################
+# Script Name	:
+# Description	:
+# Arguments		:
+# Author		: James M Conroy
+################################################################################
+
+#Creat a symbolic link between ~/.vimrc and ~/.vim/vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+
+#Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
